5f589bf71f28a4d57bbac49144a41d5ec60a8241
AlbertoGiunti <alberto.giunti@stud.unifi.it>
2023-06-10 12:49:47 +0200

    Modifica settings.py per i csrf

--- mysocial/views.py
+++ mysocial/views.py
@@ -30,9 +30,33 @@ def index(request):
 
     feed_list = list(chain(*feed))
 
-    # get all posts from users that the current user is following
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts, 'feed_list': feed_list})
+    # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    # every user that I am not following and that is not me
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list, 'feed_list': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 
 @login_required(login_url='login')
@@ -249,7 +273,22 @@ def search(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    username_profile_list = []
+
     if request.method == 'POST':
         username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)  # get all users that contain the username
 
-    return render(request, 'search.html')
+        username_profile = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'search.html',
+                  {'user_profile': user_profile, 'username_profile_list': username_profile_list})
